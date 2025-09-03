0c36889726e67614eff50dd88bb8332084c28105
syedshafinahmed <shafinahmed.cse@gmail.com>
2025-09-03 11:11:22 +0600

    added

--- jsonplaceholder.js
+++ jsonplaceholder.js
@@ -2,7 +2,24 @@
 //     console.log(result);
 
 const loadData = () => {
-    fetch('https://jsonplaceholder.typicode.com/todos/1')
-        .then(response => response.json())
+    fetch('https://jsonplaceholder.typicode.com/todos/1') // promises response
+        .then(response => response.json()) // promises json data
         .then(json => console.log(json));
 }
+
+const loadPost = () => {
+    const url = "https://jsonplaceholder.typicode.com/posts";
+    fetch(url)
+        .then(res => res.json())
+        .then(json => {
+            displayPost(json);
+            console.log(json);
+        });
+};
+
+const displayPost = (posts) =>{
+    // console.log(posts);
+    posts.forEach(post => {
+        console.log(post);
+    })
+};
